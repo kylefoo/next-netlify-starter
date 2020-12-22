c258ca7869fab756c8edbcac05b61945ace3423f
Kyle Foo <fung933@gmail.com>
2020-12-23 02:10:04 +0800

    add apollo client

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ launches }) {
   return (
     <div className="container">
       <Head>
@@ -11,13 +13,59 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="NextJS SpaceX Launches!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          version 1
         </p>
+        <div className={styles.grid}>
+        {launches.map(launch => {
+          return (
+            <a key={launch.id} href={launch.links.video_link} className={styles.card}>
+              <h3>{ launch.mission_name }</h3>
+              <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US") }</p>
+            </a>
+          );
+        })}
+        </div>
       </main>
 
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query GetLaunches {
+        launchesPast(limit: 10) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+        }
+      }
+    `
+  });
+
+  return {
+    props: {
+      launches: data.launchesPast
+    }
+  }
 }
